fc80c52b0715b3be178a11fdd7ef980842e5a92e
phandinhlan <pdl.dev@outlook.com>
2017-02-22 21:20:38 -0800

    Change routing for A1 component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,11 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './features/home.component';
 import { NotFound404Component } from './not-found404.component';
+import { A1Component } from './features/a1/a1.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'A1', loadChildren: './features/a1/a1.module#A1Module' },
+  { path: 'A1', component: A1Component },
   { path: 'A2', loadChildren: './features/a2/a2.module#A2Module' },
   { path: 'about', loadChildren: './features/about/index#AboutModule?sync=true' },
   { path: '**', component: NotFound404Component }
