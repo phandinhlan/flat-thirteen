381ed689ab7851e23e76c7e8282f197878107173
phandinhlan <pdl.dev@outlook.com>
2017-03-16 12:34:15 -0700

    Add simple gradient to better see shockwave effect TODO: figure out why the "center" isn't consistent

--- src/client/a2/main/grid/top-effect.ts
+++ src/client/a2/main/grid/top-effect.ts
@@ -20,6 +20,39 @@ class ExampleFilter extends PIXI.Filter {
   }
 }
 
+class RadialGradient extends PIXI.Filter {
+  constructor() {
+    super(
+      null,
+      `
+      varying vec2 vTextureCoord;
+
+      uniform vec4 color;
+      uniform float expand;
+      uniform vec2 center;
+      uniform float radius;
+
+      void main(void)
+      {
+        vec2 uv = vTextureCoord;
+        vec2 texCoord = uv;
+
+        float dist = distance(uv, center);
+
+        vec4 c = color - vec4(dist, dist, dist, 1.0);
+        gl_FragColor = vec4(c.x,c.y,c.z,1.0);
+      }
+      `,
+      {
+        color: { type: 'vec4' },
+        expand: { type: '1f' },
+        center: { type: 'vec2' },
+        radius: { type: '1f' },
+      }
+    )
+  }
+}
+
 class ShockwaveFilter extends PIXI.Filter {
   constructor() {
     super(
@@ -79,25 +112,37 @@ export class TopEffect {
     this.container = new PIXI.Container();
 
     let g = new PIXI.Graphics();
-    g.beginFill(0xaa3333);
-    g.drawRect(0, 0, 500, 500);
+    g.beginFill(0xffffff);
+    g.drawRect(0, 0, height, height);
     g.endFill();
     let texture = g.generateCanvasTexture();
     let sprite = new PIXI.Sprite(texture);
+    let sprite2 = new PIXI.Sprite(texture);
+    sprite2.x = height;
+
+    let rgFilter = new RadialGradient();
+    rgFilter.uniforms.color = [0.0, 1.0, 0.0, 1.0];
+    rgFilter.uniforms.center = [0.375, 0.375];
+    rgFilter.uniforms.radius = height/2;
+    rgFilter.uniforms.expand = 0.0;
+    rgFilter.uniforms.windowHeight = height;
 
     this.shockwaveFilter = new ShockwaveFilter();
-    this.shockwaveFilter.uniforms['center'] = [0.5, 0.5];
-    this.shockwaveFilter.uniforms['params'] = [10, 0.8, 0.1];
-    this.shockwaveFilter.uniforms['time'] = 0;
+    this.shockwaveFilter.uniforms.center = [0.375, 0.375];
+    this.shockwaveFilter.uniforms.params = [10, 0.8, 0.1];
+    this.shockwaveFilter.uniforms.time = 0;
     /*
     this.shockwaveFilter = new ExampleFilter();
     this.shockwaveFilter.uniforms.time = 0.0;
     */
 
     //g.filters = [this.shockwaveFilter];
-    sprite.filters = [this.shockwaveFilter];
+    //sprite.filters = [this.shockwaveFilter, rgFilter];
+    sprite.filters = [rgFilter, this.shockwaveFilter];
+    sprite2.filters = [this.shockwaveFilter];
 
     this.container.addChild(sprite);
+    this.container.addChild(sprite2);
   }
 
   getView() {
