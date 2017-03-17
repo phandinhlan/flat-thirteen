50a1b8c35d29b3e277aef7bf1cd7695ddfd97ce6
phandinhlan <pdl.dev@outlook.com>
2017-03-17 01:01:08 -0700

    More playing around with shaders

--- src/client/a2/main/grid/top-effect.ts
+++ src/client/a2/main/grid/top-effect.ts
@@ -39,8 +39,9 @@ class RadialGradient extends PIXI.Filter {
 
         float dist = distance(uv, center);
 
-        vec4 c = color - vec4(dist, dist, dist, 1.0);
-        gl_FragColor = vec4(c.x,c.y,c.z,1.0);
+        //vec4 c = color - vec4(dist, dist, dist, 1.0);
+        vec4 c = smoothstep(0.0,1.0,dist) * vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(c.x,c.y,c.z,color.w);
       }
       `,
       {
@@ -96,6 +97,47 @@ class ShockwaveFilter extends PIXI.Filter {
   }
 }
 
+class RadarFilter extends PIXI.Filter {
+  constructor() {
+    super(
+      null, 
+      `
+      varying vec2 vTextureCoord;
+
+      uniform sampler2D uSampler;
+
+      uniform float time;
+      uniform vec2 center;
+
+      void main()
+      {
+          vec2 uv = vTextureCoord;
+          vec4 finalColor;
+          
+          float r = distance(uv, center);
+          float value = smoothstep(0.0,time,r);
+
+          finalColor += value * vec4(0.0, 0.25, 1.00, 0.5);
+          //finalColor += 1.0 * vec4(0.74, 0.95, 1.00, 1.0);
+          if (value > 0.5 || value < 0.25)
+          {
+            gl_FragColor = texture2D(uSampler, uv);
+          }
+          else
+          {
+            gl_FragColor = mix(texture2D(uSampler, uv), finalColor, 0.5);
+          }
+          
+      }
+      `, 
+      {
+        time: { type: '1f' },
+        center: { type: 'vec2' },
+      }
+    );
+  }
+}
+
 
 export class TopEffect {
   renderer: PIXI.SystemRenderer;
@@ -103,6 +145,7 @@ export class TopEffect {
   width: number;
   height: number;
   shockwaveFilter: ShockwaveFilter;
+  radarFilter: RadarFilter;
 
   init(width: number, height: number) {
     this.width = width;
@@ -112,16 +155,16 @@ export class TopEffect {
     this.container = new PIXI.Container();
 
     let g = new PIXI.Graphics();
-    g.beginFill(0xffffff);
+    g.beginFill(0x1050b0);
     g.drawRect(0, 0, height, height);
     g.endFill();
     let texture = g.generateCanvasTexture();
     let sprite = new PIXI.Sprite(texture);
     let sprite2 = new PIXI.Sprite(texture);
-    sprite2.x = height;
+    //sprite2.x = height;
 
     let rgFilter = new RadialGradient();
-    rgFilter.uniforms.color = [0.0, 1.0, 0.0, 1.0];
+    rgFilter.uniforms.color = [0.0, 127.0/255.0, 1.0, 0.0];
     rgFilter.uniforms.center = [0.375, 0.375];
     rgFilter.uniforms.radius = height/2;
     rgFilter.uniforms.expand = 0.0;
@@ -136,12 +179,16 @@ export class TopEffect {
     this.shockwaveFilter.uniforms.time = 0.0;
     */
 
+    this.radarFilter = new RadarFilter();
+    this.radarFilter.uniforms.center = [0.3125, 0.3125];
+    this.radarFilter.uniforms.time = 0.0;
+
     //g.filters = [this.shockwaveFilter];
     //sprite.filters = [this.shockwaveFilter, rgFilter];
-    sprite.filters = [rgFilter, this.shockwaveFilter];
-    sprite2.filters = [this.shockwaveFilter];
+    //sprite.filters = [rgFilter, this.shockwaveFilter];
+    sprite2.filters = [this.radarFilter];
 
-    this.container.addChild(sprite);
+    //this.container.addChild(sprite);
     this.container.addChild(sprite2);
   }
 
@@ -154,6 +201,12 @@ export class TopEffect {
     if (this.shockwaveFilter.uniforms.time >= 1.0) {
         this.shockwaveFilter.uniforms.time = 0.0;
     }
+
+    this.radarFilter.uniforms.time += 0.02;
+    if (this.radarFilter.uniforms.time >= 1.0) {
+        this.radarFilter.uniforms.time = 0.0;
+    }
+
     this.renderer.render(this.container);
 
     
